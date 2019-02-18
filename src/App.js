036fc6bc8f89b03dc8fa90231d125bbda828fc51
Saravanan <saravanan@getsimpl.com>
2019-02-19 00:27:22 +0530

    Redux logger configuration

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './containers/TodoList'
 import { Provider } from 'react-redux'
-import {combineReducers, createStore} from 'redux'
+import {combineReducers, createStore, applyMiddleware} from 'redux'
 import todo from './reducers/todo'
+import { createLogger } from 'redux-logger'
 
+const loggerMiddleware = createLogger()
 const rootReducers = combineReducers({todo})
-const store = createStore(rootReducers)
+const store = createStore(rootReducers, applyMiddleware(loggerMiddleware))
 class App extends Component {
   render() {
     return (<Provider store={store}>
